22a8006087258e3bc7b133eb7008a60bb5c6116d
Ervan Ade Wijaya <89782667+ervanade@users.noreply.github.com>
2025-01-06 16:59:58 +0700

    fix

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -19,7 +19,7 @@ const Dashboard = () => {
       <div className="grid grid-cols-3 gap-6 mt-6">
         <Link to={`/discovery`}>
           <Card className="h-32 bg-gradient-to-r from-blue-600 to-sky-500 dark:bg-meta-4 !text-white flex justify-center items-center font-medium">
-            <span>Discovery</span>
+            <span className="text-center">Discovery</span>
           </Card>
         </Link>
         <Link to={`/analysee`}>
@@ -29,7 +29,7 @@ const Dashboard = () => {
         </Link>
         <Link to={`analytics`}>
           <Card className="h-32 bg-gradient-to-r from-blue-600 to-sky-500 dark:bg-meta-4 !text-white flex justify-center items-center font-medium">
-            <span>Brand Analytics</span>
+            <span className="text-center">Brand Analytics</span>
           </Card>
         </Link>
       </div>
--- src/store/axiosInterceptors.js
+++ src/store/axiosInterceptors.js
@@ -11,7 +11,7 @@ axios.interceptors.response.use(
     (error) => {
         if (
             error.response &&
-            error.response.status === 401
+            error.response.status === 401 && error.response.detail == "Token time expired: Signature has expired."
         ) {
             // Logout user
             store.dispatch(logoutUser());
