4055e947580e01a2418797ff5ae15da74df63e39
Ervan Ade Wijaya <89782667+ervanade@users.noreply.github.com>
2025-04-09 15:05:42 +0700

    fix

--- src/pages/Analyser.jsx
+++ src/pages/Analyser.jsx
@@ -131,15 +131,15 @@ const Analyser = () => {
       setLoading(false);
     } catch (error) {
       console.log(error);
-      if (error.response.status === 403) {
+      if (error?.response?.status === 403) {
         return Swal.fire({
           icon: "error",
           title: "Error",
           text: "Not Enough Credit",
         });
       } else if (
-        error.response.status === 404 ||
-        error.response.status === 400
+        error?.response?.status === 404 ||
+        error?.response?.status === 400
       ) {
         return Swal.fire({
           icon: "error",
